69592230b8c0c3037d3ab4f45d87e48286f22b35
FlashViper <61369148+FlashViper@users.noreply.github.com>
2022-04-06 16:24:45 -0600

    fixed levels don't load on mobile

--- Globals.gd
+++ Globals.gd
@@ -10,8 +10,20 @@ var camera : Camera2D
 # todo
 var levelIndex := 0
 var levels := PoolStringArray()
+#var data := """
+#Level1
+#Level2
+#Level3
+#Level4
+#Level_Water_1
+#Level_Water_2
+#Level_Water_3
+#Level_Water_4
+#../Gameplay
+#"""
 
 func _enter_tree() -> void:
+#	levels = data.split("\n", false)
 	levels = Utility.loadFileLines("res://Levels/LevelOrder.txt")
 
 func nextLevel() -> void:
